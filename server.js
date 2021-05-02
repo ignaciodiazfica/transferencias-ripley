5693d82792dfeb1bc31b7017fe2a42647d73c511
Ignacio Diaz <ignacio.diaz@engagemeapps.com>
2021-05-02 18:42:35 -0400

    fix: heroku deploy

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+app.use('/api', createProxyMiddleware({ target: 'http://immense-shore-27636.herokuapp.com', changeOrigin: true }));
+
+app.use(cors())
 app.use(express.static(__dirname + '/dist/frontend-prueba-tecnica'));
 
 app.get('/*', (req,res,next) => {
